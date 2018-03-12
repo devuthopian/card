77fd99e84a0647e8056530f523a77c3125a788fc
Mukesh Kumar <bishnois1@gmail.com>
2018-03-12 16:52:07 +0530

    Worked on tracking page.
    Worked on tracking page.

--- resources/views/users/profile/tracking2.blade.php
+++ resources/views/users/profile/tracking2.blade.php
@@ -1,5 +1,6 @@
 @extends('users.layouts.profile')
 @section('content')
+<?php $loggedUserDefaultProfile = $userResultObj->default_user_profile; ?>
 <body onload="sizing()" onresize="sizing()">
     <div class="wrapper">
         <nav class="navbar navbar-default navbar-fixed-top">
@@ -13,114 +14,68 @@
         <div class="content">
             <div class="container">
                 <!-- HTML -->
-                    <div class="tracking_cont">
-                <div class="tracker_head">
-                   <img src="{{ URL::asset('images/profile_img1.png') }}" alt="">
-                   <h1>Mr Mage is Tracking</h1>
-                   <h2>28 <span>Tracking</span></h2>
+                <div class="tracking_cont">
+                    <div class="tracker_head">
+                        <?php if(!empty($loggedUserDefaultProfile->profile_image)){ ?>
+                            <img src="{{ URL::asset('uploads/user/profile/') }}/{{$loggedUserDefaultProfile->profile_image}}">
+                        <?php }else{ ?>
+                            <img src="{{ URL::asset('images/avtar.jpg') }}">
+                        <?php } ?>
+                       <h1>{{$userResultObj->name}} is Tracking</h1>
+                       <h2>28 <span>Tracking</span></h2>
+                    </div>
+                    <?php $user_inc = 1; ?>
+                    @foreach($userResultObj->user_profiles as $userProfileObj)
+                        @foreach($userProfileObj->invitations as $invitationObj)
+                            @foreach($invitationObj->users as $userObj)
+                                <?php $defaultUserProfile = $userObj->default_user_profile; ?>
+                                        <div class="tracker_head_sec">
+                                            <div class="head_sec_cont">
+                                                <span class="dropdown_icon" data-toggle="collapse" data-target="#cards<?php echo $user_inc; ?>"><i class="fas fa-angle-down"></i></span>
+                                                <div class="head_sec_img">
+                                                    <?php if(!empty($defaultUserProfile->profile_image)){ ?>
+                                                        <img src="{{ URL::asset('uploads/user/profile/') }}/{{$defaultUserProfile->profile_image}}">
+                                                    <?php }else{ ?>
+                                                        <img src="{{ URL::asset('images/avtar.jpg') }}">
+                                                    <?php } ?>
+                                                </div>
+                                                <div class="head_sec_name">
+                                                    <h2>{{$userObj->name}}</h2>
+                                                    <?php $userCardsResultObj = $userObj->released_cards; ?>
+                                                    <h3>Released {{$userCardsResultObj->count()}} Cards</h3>                        
+                                                </div>
+                                                <div class="head_sec_detail">
+                                                    <p><span>Activity Today 16:21</span></p>
+                                                    <p>{{$defaultUserProfile->description}}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="cards_cont">
+                                            <div id="cards<?php echo $user_inc; ?>" class="collapse">
+                                                <?php 
+                                                    $userProfilesObj = $userObj->user_profiles;
+                                                    $card_inc = 0;
+                                                ?>
+                                                @foreach($userProfilesObj as $profileObj)
+                                                    @foreach($profileObj->released_cards as $cardObj)
+                                                        <div class="n_cards">
+                                                            <span class="n_cards_title">{{$cardObj->card_name}}</span>
+                                                            <img src="{{ URL::asset('uploads/card/') }}/{{$cardObj->image}}" alt="">
+                                                            <span class="n_cards_img hide" ><img src="{{ URL::asset('images/acquired.png') }}" alt=""></span>
+                                                        </div>
+                                                        <?php $card_inc++ ?>
+                                                    @endforeach
+                                                @endforeach
+                                                @if($card_inc==0)
+                                                    No Cards Available
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <?php $user_inc++; ?>
+                                @endforeach
+                            @endforeach
+                        @endforeach
                 </div>
-                <div class="tracker_head_sec">
-                   <div class="head_sec_cont">
-                   <span class="dropdown_icon" data-toggle="collapse" data-target="#demo1"><i class="fas fa-angle-down"></i></span>
-                    <div class="head_sec_img">
-                        <img src="{{ URL::asset('images/profile_img2.png') }}" alt="">
-                    </div>
-                    <div class="head_sec_name">
-                        <h2>Jane Austen</h2>
-                        <h3>Released 10 Cards</h3>                        
-                    </div>
-                    <div class="head_sec_detail">
-                        <p><span>Activity Today 16:21</span></p>
-                        <p>Latest chirp goes here (ie a fine day it was)</p>
-                    </div>
-                   </div>
-                </div>
-                <div class="cards_cont">
-                    <div id="demo1" class="collapse">
-                        <div class="n_cards">
-                            <span class="n_cards_title">Fan Forever</span>
-                            <img src="{{ URL::asset('images/header-img.jpg') }}" alt="">
-                            <span class="n_cards_img" ><img src="{{ URL::asset('images/acquired.png') }}" alt=""></span>
-                        </div>
-                        <div class="n_cards">
-                            <span class="n_cards_title">Fan Forever</span>
-                            <img src="{{ URL::asset('images/header-img.jpg') }}" alt="">
-                            <span class="n_cards_img" ><img src="{{ URL::asset('images/acquired.png') }}" alt=""></span>
-                        </div>
-                        <div class="n_cards">
-                            <span class="n_cards_title">Fan Forever</span>
-                            <img src="{{ URL::asset('images/header-img.jpg') }}" alt="">
-                            <span class="n_cards_img" ><img src="{{ URL::asset('images/acquired.png') }}" alt=""></span>
-                        </div>
-                    </div>
-                </div>
-                   <div class="head_sec_cont">
-                   <span class="dropdown_icon" data-toggle="collapse" data-target="#demo2"><i class="fas fa-angle-down"></i></span>
-                    <div class="head_sec_img">
-                        <img src="{{ URL::asset('images/profile_img2.png') }}" alt="">
-                    </div>
-                    <div class="head_sec_name">
-                        <h2>Jane Austen</h2>
-                        <h3>Released 10 Cards</h3>                        
-                    </div>
-                    <div class="head_sec_detail">
-                        <p><span>Activity Today 16:21</span></p>
-                        <p>Latest chirp goes here (ie a fine day it was)</p>
-                    </div>
-                   </div>
-                <div class="cards_cont">
-                    <div id="demo2" class="collapse">
-                        <div class="n_cards">
-                            <span class="n_cards_title">Fan Forever</span>
-                            <img src="{{ URL::asset('images/header-img.jpg') }}" alt="">
-                            <span class="n_cards_img" ><img src="images/acquired.png" alt=""></span>
-                        </div>
-                        <div class="n_cards">
-                            <span class="n_cards_title">Fan Forever</span>
-                            <img src="{{ URL::asset('images/header-img.jpg') }}" alt="">
-                            <span class="n_cards_img" ><img src="images/acquired.png" alt=""></span>
-                        </div>
-                    </div>
-                </div>
-                   <div class="head_sec_cont">
-                   <span class="dropdown_icon" data-toggle="collapse" data-target="#demo3"><i class="fas fa-angle-down"></i></span>
-                    <div class="head_sec_img">
-                        <img src="{{ URL::asset('images/profile_img2.png') }}" alt="">
-                    </div>
-                    <div class="head_sec_name">
-                        <h2>Jane Austen</h2>
-                        <h3>Released 10 Cards</h3>                        
-                    </div>
-                    <div class="head_sec_detail">
-                        <p><span>Activity Today 16:21</span></p>
-                        <p>Latest chirp goes here (ie a fine day it was)</p>
-                    </div>
-                   </div>
-                   <div class="cards_cont">
-                    <div id="demo3" class="collapse">
-                      No Cards
-                    </div>
-                   </div>
-                   <div class="head_sec_cont">
-                   <span class="dropdown_icon" data-toggle="collapse" data-target="#demo4"><i class="fas fa-angle-down"></i></span>
-                    <div class="head_sec_img">
-                        <img src="{{ URL::asset('images/profile_img2.png') }}" alt="">
-                    </div>
-                    <div class="head_sec_name">
-                        <h2>Jane Austen</h2>
-                        <h3>Released 10 Cards</h3>                        
-                    </div>
-                    <div class="head_sec_detail">
-                        <p><span>Activity Today 16:21</span></p>
-                        <p>Latest chirp goes here (ie a fine day it was)</p>
-                    </div>
-                   </div>
-                   <div class="cards_cont">
-                    <div id="demo4" class="collapse">
-                      No Cards
-                    </div>
-                   </div>
-            </div>
                 <!-- HTML - END -->
             </div>
         </div>
