18859073aeb87c395252f02c82d449d94a792cb8
Mukesh Kumar <bishnois1@gmail.com>
2018-03-19 16:08:10 +0530

    added class for stats
    added class for stats

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,23 +5,29 @@
         <div class="tracking_cont">
         	<!-- Users -->
             <div class="col-sm-4 text-center" id="users_stats">
-            	<i class="fas fa-user"></i>
-            	<div class="clearfix"></div>
-            	<strong>Users</strong> {{count($users)}}
+                <div class="admin_stats">
+                	<i class="fas fa-user"></i>
+                	<div class="clearfix"></div>
+                	<strong>Users</strong> <span class="count_class">{{count($users)}}</span>
+                </div>
             </div>
 
             <!-- Profiles -->
             <div class="col-sm-4 text-center" id="user_profiles_stats">
-            	<i class="fas fa-user-circle"></i>
-            	<div class="clearfix"></div>
-            	<strong>User Profiles</strong> {{count($user_profiles)}}
+                <div class="admin_stats">
+                	<i class="fas fa-user-circle"></i>
+                	<div class="clearfix"></div>
+                	<strong>User Profiles</strong> <span class="count_class">{{count($user_profiles)}}</span>
+                </div>
             </div>
 
             <!-- Cards -->
             <div class="col-sm-4 text-center" id="cards_stats">
-            	<i class="far fa-clone"></i>
-            	<div class="clearfix"></div>
-            	<strong>Cards</strong> {{count($user_profiles)}}
+                <div class="admin_stats">
+                	<i class="far fa-clone"></i>
+                	<div class="clearfix"></div>
+                	<strong>Cards</strong> <span class="count_class">{{count($user_profiles)}}</span>
+                </div>
             </div>
         </div>
     </div>
