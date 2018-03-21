09c6f86d2624dbc9ecb503c97583fd2e808aae1d
Mukesh Kumar <bishnois1@gmail.com>
2018-03-21 10:59:23 +0530

    Added div in connection setting page.

--- resources/views/admin/connections/index.blade.php
+++ resources/views/admin/connections/index.blade.php
@@ -14,6 +14,7 @@
             @csrf
 
             <!-- Facebook Settings -->
+            <div class="facebook_block">
                 <h4>Facebook Settings</h4>
                 <label class="col-sm-12">Client ID</label>
                 <div class="col-sm-12 clearfix">
@@ -27,40 +28,41 @@
                 <div class="col-sm-12 clearfix">
                     {{ Form::text('connectionSetting[3]', $connectionSettingsResultObj[2]->setting_value, array('id' => 'facebook_redirect_url', 'required'=> true, 'placeholder'=>'Redirect Url', 'required'=>'required')) }}
                 </div>
+            </div>
+            <!-- Twitter Settings -->
+            <div class="twitter_block">
+                <h4>Twitter Settings</h4>
+                <label class="col-sm-12">Client ID</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[4]', $connectionSettingsResultObj[3]->setting_value, array('id' => 'twitter_client_id', 'required'=> true, 'placeholder'=>'Client ID', 'required'=>'required')) }}
+                </div>
+                <label class="col-sm-12">Client Secret</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[5]', $connectionSettingsResultObj[4]->setting_value, array('id' => 'twitter_client_secret', 'required'=> true, 'placeholder'=>'Client Secret', 'required'=>'required')) }}
+                </div>
+                <label class="col-sm-12">Redirect Url</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[6]', $connectionSettingsResultObj[5]->setting_value, array('id' => 'twitter_redirect_url', 'required'=> true, 'placeholder'=>'Redirect Url', 'required'=>'required')) }}
+                </div>
+            </div>
 
             <!-- Twitter Settings -->
-
-            <h4>Twitter Settings</h4>
-            <label class="col-sm-12">Client ID</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[4]', $connectionSettingsResultObj[3]->setting_value, array('id' => 'twitter_client_id', 'required'=> true, 'placeholder'=>'Client ID', 'required'=>'required')) }}
+            <div class="google_block">
+                <h4>Google & Youtube Settings</h4>
+                <label class="col-sm-12">Client ID</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[7]', $connectionSettingsResultObj[6]->setting_value, array('id' => 'google_client_id', 'required'=> true, 'placeholder'=>'Client ID', 'required'=>'required')) }}
+                </div>
+                <label class="col-sm-12">Client Secret</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[8]', $connectionSettingsResultObj[7]->setting_value, array('id' => 'google_client_secret', 'required'=> true, 'placeholder'=>'Client Secret', 'required'=>'required')) }}
+                </div>
+                <label class="col-sm-12">Redirect Url</label>
+                <div class="col-sm-12 clearfix">
+                    {{ Form::text('connectionSetting[9]', $connectionSettingsResultObj[8]->setting_value, array('id' => 'google_redirect_url', 'required'=> true, 'placeholder'=>'Redirect Url', 'required'=>'required')) }}
+                </div>
             </div>
-            <label class="col-sm-12">Client Secret</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[5]', $connectionSettingsResultObj[4]->setting_value, array('id' => 'twitter_client_secret', 'required'=> true, 'placeholder'=>'Client Secret', 'required'=>'required')) }}
-            </div>
-            <label class="col-sm-12">Redirect Url</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[6]', $connectionSettingsResultObj[5]->setting_value, array('id' => 'twitter_redirect_url', 'required'=> true, 'placeholder'=>'Redirect Url', 'required'=>'required')) }}
-            </div>
-
-
-            <!-- Twitter Settings -->
-
-            <h4>Google & Youtube Settings</h4>
-            <label class="col-sm-12">Client ID</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[7]', $connectionSettingsResultObj[6]->setting_value, array('id' => 'google_client_id', 'required'=> true, 'placeholder'=>'Client ID', 'required'=>'required')) }}
-            </div>
-            <label class="col-sm-12">Client Secret</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[8]', $connectionSettingsResultObj[7]->setting_value, array('id' => 'google_client_secret', 'required'=> true, 'placeholder'=>'Client Secret', 'required'=>'required')) }}
-            </div>
-            <label class="col-sm-12">Redirect Url</label>
-            <div class="col-sm-12 clearfix">
-                {{ Form::text('connectionSetting[9]', $connectionSettingsResultObj[8]->setting_value, array('id' => 'google_redirect_url', 'required'=> true, 'placeholder'=>'Redirect Url', 'required'=>'required')) }}
-            </div>
-
+            
             <div class="col-sm-12 clearfix">
                 <input type="submit" value="Save">
             </div>
